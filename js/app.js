613300207fd2fbf54549c05bfe7de82011978246
lsolanodevop <leonardo.solano.hw@gmail.com>
2021-07-17 05:19:25 -0300

    Estructura inicial para aprendizaje, habilitado el debug y el lite server

---
+++ js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let num1 = 150;
+let num2 = 3200;
+function mult(num1, num2) {
+    return num1 * num2;
+}
+console.log(mult(150, 320));
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+let num1: number = 150;
+let num2: number = 3200;
+
+function mult(num1:number,num2:number){
+  return num1 * num2;
+}
+console.log(mult(150,320));
